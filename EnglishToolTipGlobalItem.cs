7ef4d30594806d3311d1db882854c5ccdfc4dd1b
Taxolotl <112824633+Taxolotl@users.noreply.github.com>
2024-06-24 19:07:28 -0400

    Add files via upload

---
+++ EnglishToolTipGlobalItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace EnglishToolTip
+{
+    internal class EnglishToolTipGlobalItem : GlobalItem
+    {
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+        {
+            string text = $"English: {(item.ModItem != null ? item.ModItem.Name : EnglishToolTip.ItemMap[item.netID])}";
+            TooltipLine line = new(Mod, Mod.Name, text);
+            line.OverrideColor = Colors.CoinSilver;
+            tooltips.Add(line);
+        }
+    }
+}
